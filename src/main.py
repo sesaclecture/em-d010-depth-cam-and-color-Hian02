eaa1c3badfe5df292c23956972f4812ef2c6e8cf
Aron <hian02@naver.com>
2026-06-18 13:46:14 +0900

    과제

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ import numpy as np
 #
 # OpenCV 함수를 활용하세요.
 def convert_to_hsv(image):
-    raise NotImplementedError
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    return hsv
 
 
 # 문제 2.
@@ -17,7 +18,7 @@ def convert_to_hsv(image):
 #
 # OpenCV 함수를 활용하세요.
 def convert_to_lab(image):
-    raise NotImplementedError
+    return cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
 
 
 # 문제 3.
@@ -44,8 +45,8 @@ def convert_to_lab(image):
 def calculate_channel_mean(
     image,
     channel_index,
-):
-    raise NotImplementedError
+): 
+    return image[:, :, channel_index].mean()
 
 
 # 문제 4.
@@ -63,7 +64,7 @@ def create_color_mask(
     lower,
     upper,
 ):
-    raise NotImplementedError
+    return cv2.inRange(image, lower, upper)
 
 
 # 문제 5.
@@ -74,4 +75,4 @@ def create_color_mask(
 #
 # 반환값은 픽셀 개수입니다.
 def count_mask_pixels(mask):
-    raise NotImplementedError
+    return np.count_nonzero(mask == 255)
